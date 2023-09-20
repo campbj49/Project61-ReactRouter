855731c9e19c1a8acc6188e39d7371b0c340b04f
John Campbell <campbj49@gmail.com>
2023-09-20 15:58:33 -0400

    boilerplate code made

---
+++ src/Food.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+/** Food: displays list of links in the router
+ * 
+ * State:
+ * -none
+ * 
+ * Props;
+ * -none
+ * 
+ * App --> Food --> Item
+ */
+ function Food(props) {
+  return (
+    <div className="Food">
+      <p>This is all food for now</p>
+    </div>
+  );
+}
+
+export default Food;
---
+++ src/VendingMachine.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+/** VendingMachine: displays list of links in the router
+ * 
+ * State:
+ * -none
+ * 
+ * Props;
+ * -none
+ * 
+ * App --> VendingMachine --> Item
+ */
+ function VendingMachine(props) {
+  return (
+    <div className="VendingMachine">
+      <p>Initial pipeline intact</p>
+    </div>
+  );
+}
+
+export default VendingMachine;
